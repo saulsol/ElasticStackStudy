c7620089738c3c918b17e9cff9db76b71723c1ab
임솔 <dlathf3210@naver.com>
2026-03-13 14:42:50 +0900

    add stop script

---
+++ stop_elasticsearch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+APP_PATH_1="/Users/imsol/nospoon/elastic/cluster1/elasticsearch-8.1.3/bin/"
+APP_PORT_1="9200"
+APP_NAME_1="node1"
+
+if [ -f ${APP_PATH_1}${APP_PORT_1}.pid ]; then
+        PID=$(cat ${APP_PATH_1}${APP_PORT_1}.pid)
+        kill $PID
+        rm ${APP_PATH_1}${APP_PORT_1}.pid
+        echo "${APP_NAME_1}(pid:$PID) is stopped"
+else
+        echo "${APP_NAME_1} is not running"
+fi
+
+
+APP_PATH_2="/Users/imsol/nospoon/elastic/cluster2/elasticsearch-8.1.3/bin/"
+APP_PORT_2="9201"
+APP_NAME_2="node2"
+
+if [ -f ${APP_PATH_2}${APP_PORT_2}.pid ]; then
+        PID=$(cat ${APP_PATH_2}${APP_PORT_2}.pid)
+        kill $PID
+        rm ${APP_PATH_2}${APP_PORT_2}.pid
+        echo "${APP_NAME_2}(pid:$PID) is stopped"
+else
+        echo "${APP_NAME_2} is not running"
+fi
---
+++ stop_kibana.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+KIBANA_PATH_1="/Users/imsol/nospoon/elastic/cluster1/kibana-8.1.3/bin/"
+KIBANA_PORT_1="5601"
+KIBANA_NAME_1="kibana1"
+
+if [ -f ${KIBANA_PATH_1}${KIBANA_PORT_1}.pid ]; then
+        PID=$(cat ${KIBANA_PATH_1}${KIBANA_PORT_1}.pid)
+        kill -15 $PID
+        rm ${KIBANA_PATH_1}${KIBANA_PORT_1}.pid
+        echo "${KIBANA_NAME_1}(pid:$PID) is stopped"
+else
+        echo "${KIBANA_NAME_1} is not running"
+fi
+
+
+KIBANA_PATH_2="/Users/imsol/nospoon/elastic/cluster2/kibana-8.1.3/bin/"
+KIBANA_PORT_2="5602"
+KIBANA_NAME_2="kibana2"
+
+if [ -f ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid ]; then
+        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
+        kill -15 $PID
+        rm ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid
+        echo "${KIBANA_NAME_2}(pid:$PID) is stopped"
+else
+        echo "${KIBANA_NAME_2} is not running"
+fi

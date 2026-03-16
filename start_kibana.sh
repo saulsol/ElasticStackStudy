f292843fb9b519474602925b8c283bfbe5cf7474
임솔 <dlathf3210@naver.com>
2026-03-16 09:06:11 +0900

    change script

--- start_kibana.sh
+++ start_kibana.sh
@@ -12,15 +12,15 @@ else
 fi
 
 
-KIBANA_PATH_2="/Users/imsol/nospoon/elastic/cluster2/kibana-8.1.3/bin/"
-KIBANA_PORT_2="5602"
-KIBANA_NAME_2="kibana2"
-
-if [ -f ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid ]; then
-        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
-        echo "${KIBANA_NAME_2}(pid:$PID) is running"
-else
-        nohup ${KIBANA_PATH_2}kibana > ${KIBANA_PATH_2}${KIBANA_PORT_2}.log 2>&1 & echo $! > ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid
-        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
-        echo "${KIBANA_NAME_2}(pid:$PID) is start"
-fi
+#KIBANA_PATH_2="/Users/imsol/nospoon/elastic/cluster2/kibana-8.1.3/bin/"
+#KIBANA_PORT_2="5602"
+#KIBANA_NAME_2="kibana2"
+#
+#if [ -f ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid ]; then
+#        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
+#        echo "${KIBANA_NAME_2}(pid:$PID) is running"
+#else
+#        nohup ${KIBANA_PATH_2}kibana > ${KIBANA_PATH_2}${KIBANA_PORT_2}.log 2>&1 & echo $! > ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid
+#        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
+#        echo "${KIBANA_NAME_2}(pid:$PID) is start"
+#fi

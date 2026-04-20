c417edc219782e25d10d1ee6c926b0b9d98181ad
임솔 <dlathf3210@naver.com>
2026-04-20 14:46:19 +0900

    add logstash conf file text

--- stop_kibana.sh
+++ stop_kibana.sh
@@ -14,15 +14,15 @@ else
 fi
 
 
-KIBANA_PATH_2="/Users/imsol/nospoon/elastic/cluster2/kibana-8.1.3/bin/"
-KIBANA_PORT_2="5602"
-KIBANA_NAME_2="kibana2"
-
-if [ -f ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid ]; then
-        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
-        kill -15 $PID
-        rm ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid
-        echo "${KIBANA_NAME_2}(pid:$PID) is stopped"
-else
-        echo "${KIBANA_NAME_2} is not running"
-fi
+#KIBANA_PATH_2="/Users/imsol/nospoon/elastic/cluster2/kibana-8.1.3/bin/"
+#KIBANA_PORT_2="5602"
+#KIBANA_NAME_2="kibana2"
+#
+#if [ -f ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid ]; then
+#        PID=$(cat ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid)
+#        kill -15 $PID
+#        rm ${KIBANA_PATH_2}${KIBANA_PORT_2}.pid
+#        echo "${KIBANA_NAME_2}(pid:$PID) is stopped"
+#else
+#        echo "${KIBANA_NAME_2} is not running"
+#fi

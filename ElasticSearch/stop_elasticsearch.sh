b661e043f2fcb19d6992ec2d260d4c30ea703968
임솔 <dlathf3210@naver.com>
2026-04-23 16:38:25 +0900

    update config

--- ElasticSearch/stop_elasticsearch.sh
+++ ElasticSearch/stop_elasticsearch.sh
@@ -14,15 +14,15 @@ else
 fi
 
 
-APP_PATH_2="/Users/imsol/nospoon/elastic/cluster2/elasticsearch-8.1.3/bin/"
-APP_PORT_2="9204"
-APP_NAME_2="node5"
-
-if [ -f ${APP_PATH_2}${APP_PORT_2}.pid ]; then
-        PID=$(cat ${APP_PATH_2}${APP_PORT_2}.pid)
-        kill $PID
-        rm ${APP_PATH_2}${APP_PORT_2}.pid
-        echo "${APP_NAME_2}(pid:$PID) is stopped"
-else
-        echo "${APP_NAME_2} is not running"
-fi
+#APP_PATH_2="/Users/imsol/nospoon/elastic/cluster2/elasticsearch-8.1.3/bin/"
+#APP_PORT_2="9204"
+#APP_NAME_2="node5"
+#
+#if [ -f ${APP_PATH_2}${APP_PORT_2}.pid ]; then
+#        PID=$(cat ${APP_PATH_2}${APP_PORT_2}.pid)
+#        kill $PID
+#        rm ${APP_PATH_2}${APP_PORT_2}.pid
+#        echo "${APP_NAME_2}(pid:$PID) is stopped"
+#else
+#        echo "${APP_NAME_2} is not running"
+#fi
